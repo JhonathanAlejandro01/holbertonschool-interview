39ce0e598f343c760938c0b239d556a3d91562cd
Jhonathan Alejandro <joaleanco@hotmail.com>
2021-05-06 21:58:30 -0500

    fix space

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -4,12 +4,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: parent node
- * @value: integer for the new node
- *
- * Return: a pointer to the new node, or NULL on failure
- */
+* binary_tree_node - creates a binary tree node
+* @parent: parent node
+* @value: integer for the new node
+* Return: a pointer to the new node, or NULL on failure
+*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new;

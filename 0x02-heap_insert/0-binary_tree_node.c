5aaae7ddf04f57bddc8884415d057bf0d6742ff7
JhonathanAlejandro01 <joaleanco@hotmail.com>
2021-05-06 22:58:17 -0500

    add repositorie

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -12,7 +12,6 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *new;
-
 new = malloc(sizeof(binary_tree_t));
 if (new == NULL)
 return (NULL);
@@ -20,6 +19,5 @@ new->n = value;
 new->parent = parent;
 new->left = NULL;
 new->right = NULL;
-
 return (new);
 }
